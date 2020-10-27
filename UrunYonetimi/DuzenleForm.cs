2ae25c159095c7ee6e84764bcd697b1352bad49e
mustafa serif çelenk <juliuscesar77@hotmail.com>
2020-10-27 16:08:20 +0300

    Add project files.

---
+++ UrunYonetimi/DuzenleForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UrunYonetimi
+{
+    public partial class DuzenleForm : Form
+    {
+        public DuzenleForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
